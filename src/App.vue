e57d8f3f23dfb9b6aefdafa672e44b3f6ba7b016
panguangjian <18131641544@163.com>
2020-03-19 16:39:21 +0800

    整体界面的完善

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/about">About</router-link>
-        </div>
         <router-view/>
     </div>
 </template>
 
-<style lang="scss">
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
+<script>
+    export default {
+        name: "app",
+        data(){
+            return{
             }
         }
+    }
+</script>
+
+<style lang="scss">
+    .copyright {
+        font-size: .22rem;
+        line-height: .42667rem;
+        color: #999;
+    }
+    p {
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import axios from 'axios'
+import Vehistogram from 'v-charts/lib/histogram.common'
+import user from '@/js/global'
+import { Button,Tabbar, TabbarItem,Popup,Icon, NoticeBar,Divider,Uploader,Field,Form,Picker,Loading,Overlay,Sticky,DatetimePicker
+,Cell,CellGroup,NavBar } from 'vant';
 
-Vue.use(Vant);
-Vue.config.productionTip = false
+Vue.use(Button).use(Tabbar).use(TabbarItem).use(Popup).use(Icon).use(NoticeBar)
+        .use(Divider).use(Uploader).use(Field).use(Form).use(Picker).use(Loading)
+        .use(Overlay).use(Sticky).use(DatetimePicker).use(Cell).use(CellGroup)
+        .use(NavBar);
+Vue.component(Vehistogram.name, Vehistogram);
+Vue.prototype.$user = user;
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://192.168.43.127:8082/graduation';
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+});
